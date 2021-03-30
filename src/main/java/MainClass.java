24c702c99f700dd54f0544fd7afd8b49bd363120
vfomin2904 <vova-fomin97@mail.ru>
2021-03-30 09:45:29 +0300

    hw7

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,20 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainClass {
     public static void main(String[] args) {
-
+        try {
+            TestClass.start(MyClass.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
